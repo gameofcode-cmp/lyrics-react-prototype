39141d73ab271ee9eb7b67e0e2df5b063ed9b646
gameofcode-cmp <christo@gameofcode.co.uk>
2025-04-14 15:34:29 +0100

    tailwindstyling linting and better app root

--- react-from-scratch/src/app.jsx
+++ react-from-scratch/src/app.jsx
@@ -1,15 +1,23 @@
 import ReactDOM from 'react-dom/client'
+import './app.css'
 
-function App() {
+//transform.tools to convert to object format
+const theme = {
+    primary: 'yellow',
+    secondary: 'red'
+}
+
+export function App() {
     return(
         <>
             <div>
-                <h2>
+                <h1 className="font-mono">I hope today goes better</h1>
+                <h2 className="font-mono text-4xl" style={{ background: theme.primary , color: 'red'}}>
                     Howdy!, Happy{' '} {new Date().toLocaleDateString('en-UK', {weekday: 'long'})}
                 </h2>
                 <p>I hope today goes better</p>
             </div>
-            <div>
+            <div className="font-mono text-4xl">
                 This is a secondary div block the weird tag at top is shorthand for react create fragment
             </div>
             <div>
@@ -30,7 +38,3 @@ function Greeting ({children}) {
     return <h2 className='greeting'>{children}</h2>
 }
 
-const app = document.getElementById('app')
-const root = ReactDOM.createRoot(app)
-
-root.render(<App />)
--- react-from-scratch/vite.config.js
+++ react-from-scratch/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+
 
 export default defineConfig({
     plugins: [
-        viteReact()
+        react(), tailwindcss()
     ]
 })
